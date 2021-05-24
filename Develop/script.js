d12746970bd30828995db5ef4498e8d4e770d7e6
Rayell McMahan <rmcmahan1@hotmail.com>
2021-05-23 20:11:50 -0700

    adding DOM elements

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,11 @@
 // Assignment code here
 
+const randomGen = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  symbol: getRandomSymbol,
+  number: getRandomNumber,
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -20,12 +26,19 @@ function getRandomNumber() {
 
 function getRandomSymbol() {
   const symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-   return symbols[0];
+   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 
-
-
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEL = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const symbolsEl = document.getElementById('symbols');
+const numbersEl = document.getElementById('numbers');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 //function to get random 
 //four functions for each of the variables that use getrandom function to input that variable

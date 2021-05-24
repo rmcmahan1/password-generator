d7c79e287ee4e564ab7bdb252e999b64ef57883d
Rayell McMahan <rmcmahan1@hotmail.com>
2021-05-23 21:44:39 -0700

    updated README

--- Develop/script.js
+++ Develop/script.js
@@ -27,16 +27,15 @@ function getRandomNumber() {
 function getRandomSymbol() {
   const symbols = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
+};
 
 //DOM elements
 const resultEl = document.getElementById('result');
 const lengthEL = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
-const symbolsEl = document.getElementById('symbols');
-const numbersEl = document.getElementById('numbers');
+const symbolEl = document.getElementById('symbol');
+const numberEl = document.getElementById('number');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
@@ -55,6 +54,9 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword) 
 
 
+//Generate password function
+
+console.log(writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # password-generator
+
+## Description
+This is a password generator developed using HTML, CSS, and Javascript. When clicking the Generate Button feature, it will provide a random password that meets the acceptance criteria. It will contain a lowercase, uppercase, symbol and number. I defined these by the use of CharCode.
+
+## Screenshot
+
+![Screenshot](pwgenscreenshot.jpg)
+
